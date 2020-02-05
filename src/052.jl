40df730bfa488a8a59088d193049afe1767fb06c
Josh Robinson <gnujosh@gmail.com>
2020-02-05 10:48:37 -0500

    Sped up solution for 52

--- src/052.jl
+++ src/052.jl
@@ -8,7 +8,7 @@ using ProjectEulerSolutions
 
 # Check sets of digits to see if they are equivalent, but the trick is to cut
 # down the search range from 100 to 166, 1000 to 1666, etc.  This becomes much
-# simpler with the use of Sets and the built-in digits function.
+# simpler with the use of sort and the built-in digits function.
 function p052solution_builtins(n::Integer=3)::Integer
 
     for factor_power in 2:n
@@ -17,11 +17,11 @@ function p052solution_builtins(n::Integer=3)::Integer
         # Upper bound must be the same number of digits
         x_end = div(10^(factor_power + 1), 6)
         for x in x_start:x_end
-            digit_set = Set(digits(x * 2))
-            if digit_set == Set(digits(x * 3)) &&
-               digit_set == Set(digits(x * 4)) &&
-               digit_set == Set(digits(x * 5)) &&
-               digit_set == Set(digits(x * 6)) &&
+            digit_set = sort(digits(x * 2))
+            if digit_set == sort(digits(x * 3)) &&
+               digit_set == sort(digits(x * 4)) &&
+               digit_set == sort(digits(x * 5)) &&
+               digit_set == sort(digits(x * 6))
                 return x
             end
         end
@@ -29,29 +29,6 @@ function p052solution_builtins(n::Integer=3)::Integer
     return -1
 end
 
-# Explicitly converts each digit using mod and div, can exit more quickly on a
-# per-digit basis.
-function digit_helper(x::Integer, n_digits::Integer)::Bool
-    digs = zeros(Integer, 10)
-    for m in 1:5
-        num = x * (m + 1)
-        for j in 1:n_digits
-            num2 = (num % 10) + 1
-            digs[num2] += 1
-            if digs[num2] != m
-                return false
-            end
-            num = div(num, 10)
-        end
-    end
-    for digit in digs
-        if 0 < digit < 5
-            return false
-        end
-    end
-
-    return true
-end
 
 # Explicitly handles the digit checking rather than using built-ins.  Faster,
 # but more complicated.
@@ -61,8 +38,27 @@ function p052solution_explicit(n::Integer=3)::Integer
         x_start = 10^(n_digits - 1)
         # Upper bound must be the same number of digits
         x_end = div(10^n_digits, 6)
+        digs = zeros(Int8, 10)
         for x in x_start:x_end
-            if digit_helper(x, n_digits)
+            fill!(digs, 0)
+            broke_early = false
+            for m in 1:5
+                num = x * (m + 1)
+                for j in 1:n_digits
+                    num2 = (num % 10) + 1
+                    digs[num2] += 1
+                    # Break early if one of the multipliers doesn't match
+                    if digs[num2] != m
+                        broke_early = true
+                        break
+                    end
+                    num = div(num, 10)
+                end
+                if broke_early  # Inner loop broke early
+                    break
+                end
+            end
+            if ! broke_early
                 return x
             end
         end
